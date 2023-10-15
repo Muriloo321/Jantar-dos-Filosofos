d3b90bd8d0802fe79d44e9387002b16c42e7eb5d
Murilo Moreno <124192765+Muriloo321@users.noreply.github.com>
2023-10-14 23:14:04 -0300

    Update JantarDosFilosofos.java

--- src/jantarfilosofos/JantarDosFilosofos.java
+++ src/jantarfilosofos/JantarDosFilosofos.java
@@ -11,7 +11,7 @@ public class JantarDosFilosofos {
 
 	    public static void main(String[] args) {
 	        for (int i = 0; i < Filosofos; i++) {
-	        	Garfos[i] = new Semaphore(1); // Inicializa cada garfo como disponível (semaforo com permissão 1)
+	        	Garfos[i] = new Semaphore(1); 
 	        }
 
 	        Thread[] filosofos = new Thread[Filosofos];
@@ -22,17 +22,17 @@ public class JantarDosFilosofos {
 	        }
 
 	        try {
-	            Thread.sleep(5000); // Aguarda por um tempo da sua escolha
+	            Thread.sleep(5000); 
 	        } catch (InterruptedException e) {
 	            e.printStackTrace();
 	        }
 
-	        // Interrompe as threads dos filósofos para encerrar a execução
+	      
 	        for (Thread filosofo : filosofos) {
 	            filosofo.interrupt();
 	        }
 
-	        // Mostra estatísticas no final
+	        
 	        for (int i = 0; i < Filosofos; i++) {
 	            System.out.println(Nomes[i] + " comeu " + Filosofo.vezesComeu[i] + " vezes e pensou " + Filosofo.vezesPensou[i] + " vezes.");
 	        }
@@ -53,7 +53,7 @@ public class JantarDosFilosofos {
 
 	            if (id % 2 == 0) {
 	            	Garfos[garfoEsquerda].acquire();
-	                Thread.sleep(Random.nextInt(1000)); // Tempo aleatório para simular o tempo que o filósofo gasta pensando
+	                Thread.sleep(Random.nextInt(1000)); 
 	                Garfos[garfoDireita].acquire();
 	            } else {
 	            	Garfos[garfoDireita].acquire();
@@ -71,7 +71,7 @@ public class JantarDosFilosofos {
 	        public void run() {
 	            try {
 	                while (!Thread.interrupted()) {
-	                	 int acaoAleatoria = Random.nextInt(3); // 0, 1 ou 2
+	                	 int acaoAleatoria = Random.nextInt(3); 
 	                     if (acaoAleatoria == 0) {
 	                         System.out.println(Nomes[id] + " está pensando.");
 	                         Thread.sleep(Random.nextInt(500));
@@ -89,7 +89,7 @@ public class JantarDosFilosofos {
 	                     }
 	                }
 	            } catch (InterruptedException e) {
-	                // A thread foi interrompida, termina a execução
+
 	            }
 	        }
 	    }
